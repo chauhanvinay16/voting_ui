5340e567b5be186eba599965b6dea2fdd462ec7a
vinay chauhan <bcavinay16@gmail.com>
2024-03-01 13:00:20 +0530

    change done

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,77 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(VotingApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class VotingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Voting App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: VotingScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-
+class VotingScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _VotingScreenState createState() => _VotingScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _VotingScreenState extends State<VotingScreen> {
+  int _yesVotes = 0;
+  int _noVotes = 0;
+
+  void _voteYes() {
+    setState(() {
+      _yesVotes++;
+    });
+  }
+
+  void _voteNo() {
+    setState(() {
+      _noVotes++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('demo'),
+        title: Text('Vote Now'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Do you agree with this statement?',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(onPressed: _voteYes, child: Text("YES")),
+                ElevatedButton(onPressed: _voteNo, child: Text("NO"))
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Yes Votes: $_yesVotes',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'No Votes: $_noVotes',
+              style: TextStyle(fontSize: 18.0),
+            ),
+          ],
         ),
       ),
     );
